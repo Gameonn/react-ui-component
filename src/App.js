87b8299d2322c28d06065a4e6bc1ffbb60f359af
Gameonn <jindal.ankit89@gmail.com>
2022-05-04 11:28:23 +0530

    Optimize code and add new button types

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import Page from "./Page";
+import Page from "./components/Page";
 import "./App.css";
 
 const App = () => {
-  console.log("App");
   return (
     <div className="App">
       <Page />
       <center>
         <strong>
-          Created By: <a href="https://github.com/Gameonn">Gameonn</a>{" "}
+          Created By: <a href="https://github.com/Gameonn">Gameonn</a>
         </strong>
         - devChallenges.io
       </center>
---
+++ src/components/Main.js
@@ -0,0 +1,152 @@
+import Button from "./Button";
+import { FlexboxGrid, Divider } from "rsuite";
+import { nanoid } from "nanoid";
+
+let styleObj = {
+  display: "block",
+  height: "40px"
+};
+const variantTypes = ["normal", "outline", "bold", "text"];
+const colorTypes = ["default", "primary", "danger", "warning"];
+const sizeTypes = ["xs", "sm", "md", "lg"];
+const startIconTypes = ["home", "bookmarks", "bolt", "book"];
+const endIconTypes = ["toys", "whatshot", "weekend", "vignette"];
+
+const Main = () => {
+  return (
+    <div>
+      <h4>Buttons</h4> <br />
+      <div className="show-grid">
+        <b> Normal </b>
+        <FlexboxGrid justify="space-around">
+          {variantTypes.map(type => (
+            <FlexboxGrid.Item colspan={6} key={nanoid()}>
+              <small style={styleObj}> {`<Button variant="${type}" />`} </small>
+              <Button variant={type} />
+            </FlexboxGrid.Item>
+          ))}
+        </FlexboxGrid>
+
+        <Divider />
+        <b> Colors </b>
+        <FlexboxGrid justify="space-around">
+          {colorTypes.map(type => (
+            <FlexboxGrid.Item colspan={6} key={nanoid()}>
+              <small style={styleObj}> {`<Button color="${type}" />`} </small>
+              <Button color={type} />
+            </FlexboxGrid.Item>
+          ))}
+        </FlexboxGrid>
+        <Divider />
+        <b> Size </b>
+        <FlexboxGrid justify="space-around">
+          {sizeTypes.map(type => (
+            <FlexboxGrid.Item colspan={6} key={nanoid()}>
+              <small style={styleObj}> {`<Button size="${type}" />`} </small>
+              <Button size={type} />
+            </FlexboxGrid.Item>
+          ))}
+        </FlexboxGrid>
+        <Divider />
+        <b>Icons (startIcon)</b>
+        <FlexboxGrid justify="space-around">
+          {startIconTypes.map((type, i) => (
+            <FlexboxGrid.Item colspan={6} key={nanoid()}>
+              <small style={styleObj}>
+                {`<Button startIcon="${type}" color="${colorTypes[i]}" />`}
+              </small>
+              <Button startIcon={type} color={colorTypes[i]} />
+            </FlexboxGrid.Item>
+          ))}
+        </FlexboxGrid>
+        <Divider />
+        <b>Icons (endIcon)</b>
+        <FlexboxGrid justify="space-around">
+          {endIconTypes.map((type, i) => (
+            <FlexboxGrid.Item colspan={6} key={nanoid()}>
+              <small style={styleObj}>
+                {`<Button endIcon="${type}" color="${colorTypes[i]}" />`}
+              </small>
+              <Button endIcon={type} color={colorTypes[i]} />
+            </FlexboxGrid.Item>
+          ))}
+        </FlexboxGrid>
+        <Divider />
+        <b>Outline colors</b>
+        <FlexboxGrid justify="space-around">
+          {colorTypes.map((type, i) => (
+            <FlexboxGrid.Item colspan={6} key={nanoid()}>
+              <small style={styleObj}>
+                {`<Button variant="outline" color="${type}" />`}
+              </small>
+              <Button color={type} variant="outline" />
+            </FlexboxGrid.Item>
+          ))}
+        </FlexboxGrid>
+        <Divider />
+        <b>Text colors</b>
+        <FlexboxGrid justify="space-around">
+          {colorTypes.map((type, i) => (
+            <FlexboxGrid.Item colspan={6} key={nanoid()}>
+              <small style={styleObj}>
+                {`<Button variant="text" color="${type}" />`}
+              </small>
+              <Button color={type} variant="text" />
+            </FlexboxGrid.Item>
+          ))}
+        </FlexboxGrid>
+        <Divider />
+        <b>Bold colors</b>
+        <FlexboxGrid justify="space-around">
+          {colorTypes.map((type, i) => (
+            <FlexboxGrid.Item colspan={6} key={nanoid()}>
+              <small style={styleObj}>
+                {`<Button variant="bold" color="${type}" />`}
+              </small>
+              <Button color={type} variant="bold" />
+            </FlexboxGrid.Item>
+          ))}
+        </FlexboxGrid>
+        <Divider />
+        <b>Disabled</b>
+        <FlexboxGrid justify="space-around">
+          {colorTypes.map((type, i) => (
+            <FlexboxGrid.Item colspan={6} key={nanoid()}>
+              <small style={styleObj}>
+                {`<Button disabled color="${type}" />`}
+              </small>
+              <Button color={type} disabled />
+            </FlexboxGrid.Item>
+          ))}
+        </FlexboxGrid>
+        <Divider />
+        <b>Text Disabled</b>
+        <FlexboxGrid justify="space-around">
+          {colorTypes.map((type, i) => (
+            <FlexboxGrid.Item colspan={6} key={nanoid()}>
+              <small style={styleObj}>
+                {`<Button disabled variant="text" color="${type}" />`}
+              </small>
+              <Button color={type} variant="text" disabled />
+            </FlexboxGrid.Item>
+          ))}
+        </FlexboxGrid>
+        <Divider />
+        <b>Disable Shadow</b>
+        <FlexboxGrid justify="space-around">
+          {colorTypes.map((type, i) => (
+            <FlexboxGrid.Item colspan={6} key={nanoid()}>
+              <small style={styleObj}>
+                {`<Button disableShadow color="${type}" />`}
+              </small>
+              <Button color={type} disableShadow />
+            </FlexboxGrid.Item>
+          ))}
+        </FlexboxGrid>
+        <Divider />
+      </div>
+    </div>
+  );
+};
+
+export default Main;
